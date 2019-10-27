6cfb0e36844c6e364dbb55cd236f84ba513b6a37
BoytsovVA <vladboi8@gmail.com>
2019-10-27 21:47:57 +0300

    BoytsovVA Version1

--- Сысоев/base/postfix.h
+++ Сысоев/base/postfix.h
@@ -8,17 +8,35 @@ using namespace std;
 
 class TPostfix
 {
-  string infix;
-  string postfix;
+	string infix;  
+	string postfix; 
+	bool IsOperator(char v);
+	bool IsOperand(char v);
+	int Priority(char a, char b);
+	void InputStr(string& v);
+	bool CheckOperations();
+	bool CheckBrackets();
+	void SplitStr(string& str);
 public:
-  TPostfix()
-  {
-    infix = "a + b";
-  }
-  string GetInfix() { return infix; }
-  string GetPostfix() { return postfix; }
-  string ToPostfix();
-  double Calculate(); // Ввод переменных, вычисление по постфиксной форме
+	TPostfix(string tmp = "")
+	{
+		infix = tmp; 
+		postfix = "";
+	}
+	TPostfix(TPostfix& v)
+	{
+		infix = v.infix;
+		postfix = v.postfix;
+	}
+	void Set(string& _infix)  //Сеттер
+	{
+		infix = _infix;
+		postfix = "";
+	}
+	string GetInfix() { return infix; }  //Геттер инфиксной формы
+	string GetPostfix() { return postfix; }  //Геттер постфиксной формы
+	string ToPostfix();  //Перевод в постфиксную форму
+	double Calculate(); // Ввод переменных, вычисление по постфиксной форме
 };
 
 #endif
